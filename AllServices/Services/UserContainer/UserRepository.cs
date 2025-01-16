82e0446db99ddb65a575e24fa99f111be745163c
pritom-dip <pritomdip010@gmail.com>
2025-01-16 11:21:34 +0000

    Add Base repository class

--- AllServices/Services/UserContainer/UserRepository.cs
+++ AllServices/Services/UserContainer/UserRepository.cs
@@ -2,49 +2,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AllServices.Services.RepositoryContainer;
 using DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Models;
 
 namespace AllServices.Services.UserContainer
 {
-    public class UserRepository : IUserRepository
+    public class UserRepository : Repository<User>, IUserRepository
     {
         private readonly ApplicationDbContext _userRepo;
 
-        public UserRepository(ApplicationDbContext context)
+        public UserRepository(ApplicationDbContext context) : base(context)
         {
             _userRepo = context;
-        }
-
-        public async Task<User> Create(User user)
-        {
-            await _userRepo.Users.AddAsync(user);
-            await _userRepo.SaveChangesAsync();
-            return user;
-        }
-
-        public async Task<User?> Delete(User user)
-        {
-            _userRepo.Users.Remove(user);
-            await _userRepo.SaveChangesAsync();
-            return user;
-        }
-
-        public IQueryable<User> Get()
-        {
-            return _userRepo.Users.AsQueryable();
-        }
-
-        public async Task<User?> GetById(int id)
-        {
-            return await _userRepo.Users.FirstOrDefaultAsync(x => x.Id == id);
-        }
-
-        public async Task<User> Update(User user)
-        {
-            await _userRepo.SaveChangesAsync();
-            return user;
         }
 
         public async Task<bool> UserExists(int id)
